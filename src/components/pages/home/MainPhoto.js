9c6feb65a6072cab2a9c8032d8456933e3234c66
Arm1700 <bukbok1@gmail.com>
2024-10-31 11:54:10 +0400

    mainPhoto

--- src/components/pages/home/MainPhoto.js
+++ src/components/pages/home/MainPhoto.js
@@ -1,33 +1,39 @@
 export default function MainPhoto({image, text1, text2}) {
 
     return (
-        <section className="flex uppercase justify-center middle:h-[450px] md:h-[650px] h-[500px] flex-col text-pseudo px-5 relative overflow-hidden">
-            <video
-                autoPlay
-                muted
-                loop
-                className="absolute top-0 left-0 min-w-full min-h-full object-cover"
-                // playsinline=""
-                // class="mui-1se970o-video"
-                // controls=""
-                // data-automation="VideoPlayer"
-                // height="100%"
-                // loop=""
-                // muted=""
-                // autoplay=""
-                // width="100%"
-                // poster="https://www.shutterstock.com/shutterstock/videos/1093083365/thumb/9.jpg?ip=x480"
-                poster="https://eduma.thimpress.com/demo-udemy/wp-content/uploads/sites/93/2024/04/banner-home-2.jpg"
-                // preload="auto"
-                // aria-label="video-player"
-                // controlslist="nodownload"
-            >
-                <source
-                    src={image}
-                    type="video/webm"
-                />
+        <section style={{
+            backgroundImage: 'url(DSC07939.jpg)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100vh',
+            width: '100%',
+        }} className="flex uppercase justify-center middle:h-[450px] md:h-[650px] h-[700px] flex-col text-pseudo px-5 relative overflow-hidden">
 
-            </video>
+            {/*<video*/}
+            {/*    autoPlay*/}
+            {/*    muted*/}
+            {/*    loop*/}
+            {/*    className="absolute top-0 left-0 min-w-full min-h-full object-cover"*/}
+            {/*    // playsinline=""*/}
+            {/*    // class="mui-1se970o-video"*/}
+            {/*    // controls=""*/}
+            {/*    // data-automation="VideoPlayer"*/}
+            {/*    // height="100%"*/}
+            {/*    // loop=""*/}
+            {/*    // muted=""*/}
+            {/*    // autoplay=""*/}
+            {/*    // width="100%"*/}
+            {/*    poster="DSC07939.jpg"*/}
+            {/*    // preload="auto"*/}
+            {/*    // aria-label="video-player"*/}
+            {/*    // controlslist="nodownload"*/}
+            {/*>*/}
+            {/*    <source*/}
+            {/*        src="DSC07939.jpg"*/}
+            {/*        type="video/webm"*/}
+            {/*    />*/}
+
+            {/*</video>*/}
             <div className="popularDiv relative absoluteP middle:top-[175px] top-[125px] left-[50%]">
                 <div className="relative z-10 bg-white text-color12 middle:w-[520px] w-full p-[30px]">
                     <h1 className="font-roboto-slab no-underline whitespace-no-wrap min-h-0 min-w-0 text-left leading-60 tracking-normal font-bold text-4xl sm:text-5xl backdrop-filter-none filter-none transform-origin-center opacity-100 translate-x-0 translate-y-0 visible">
